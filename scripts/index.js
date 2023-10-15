3240b38f33d75a4089d9775ace3808eb5fe08bf7
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-10-15 18:05:57 +0330

    newest page structure and styles

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,12 @@ const profileElem = $.querySelector(".header__panel-profile");
 const headerMenuElem = $.querySelector(".header__menu");
 
 profileElem.addEventListener("click", () => {
-    console.log('show')
     overlayElem.classList.add("show")
     headerMenuElem.classList.add("show")
 })
 
 overlayElem.addEventListener("click", () => {
-    console.log('hide')
     overlayElem.classList.remove("show")
     headerMenuElem.classList.remove("show")
 })
 
-window.addEventListener("load", () => {
-    const swiper = new Swiper('.nav__items', {
-        // Optional parameters
-        direction: 'horizontal',
-        slidesPerView: 'auto',
-        spaceBetween: 12,
-        // centeredSlides: true,
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
-})
