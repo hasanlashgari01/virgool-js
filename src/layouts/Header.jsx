edbd40dc2109ef0b7c9b9ce4f846e347921e4baa
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-11-10 17:55:56 +0330

    auth links in header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,7 +1,7 @@
-import { Link } from "react-router-dom";
 import LinkItem from "../components/templates/LinkItem";
 import ProfileDropDown from "../components/templates/ProfileDropDown";
 import NewPost from "../components/templates/NewPost";
+import { Link } from "react-router-dom";
 
 const items = [
 	{ id: 1, link: "/", name: "صفحه اصلی" },
@@ -13,13 +13,21 @@ const items = [
 const Header = () => {
 	return (
 		<header>
-			<div className="flex justify-between items-center">
+			<div className="flex justify-between items-center py-6">
 				<div className="flex gap-2.5">
 					<img src="https://virgool.io/images/icon.svg" alt="آیکون ویرگول" />
 					<NewPost />
 				</div>
 				<div>
-					<ProfileDropDown />
+					{/* <ProfileDropDown /> */}
+					<div className="flex gap-3 items-center">
+						<Link to="/auth/login" className="text-blue-500 bg-white py-2 px-6">
+							ورود
+						</Link>
+						<Link to="/auth/register" className="bg-blue-500 text-white py-2 px-6 rounded-md">
+							ثبت نام
+						</Link>
+					</div>
 				</div>
 			</div>
 			<nav>
