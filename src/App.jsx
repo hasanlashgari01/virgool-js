0b9fc5f6ceed9cd044b43e06e43411dc32060e3e
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-12-02 21:31:07 +0330

    auth context done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useRoutes } from "react-router-dom";
 import { routes } from "./routes/routes.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 function App() {
 	const router = useRoutes(routes);
 
-	return <>{router}</>;
+	return <AuthProvider>{router}</AuthProvider>;
 }
 
 export default App;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from "react";
+import propTypes from "prop-types";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+	const [defaultValue, setDefaultValue] = useState([
+		{
+			isLoggedIn: false,
+			token: null,
+			userInfos: null,
+			login: () => {},
+			logout: () => {},
+		},
+	]);
+
+	const loginHandler = (token) => {
+		setDefaultValue({ ...defaultValue, isLoggedIn: true, token });
+		localStorage.setItem("user", JSON.stringify({ token }));
+	};
+
+	const logoutHandler = () => {
+		setDefaultValue({ ...defaultValue, token: null, userInfos: null });
+		localStorage.removeItem("user");
+	};
+
+	return (
+		<AuthContext.Provider value={{ defaultValue, loginHandler, logoutHandler }}>{children}</AuthContext.Provider>
+	);
+};
+
+AuthProvider.propTypes = {
+	children: propTypes.object,
+};
+
+export default AuthProvider;
