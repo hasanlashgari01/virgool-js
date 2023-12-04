252d79b17f289864db2e28f51c2ef9000c284dda
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-12-04 20:27:25 +0330

    user can create comment

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
-import { BASE_URL } from "../services/virgoolApi";
+import { BASE_URL, TOKEN_ADMIN } from "../services/virgoolApi";
 import PostDetails from "../components/templates/PostDetails";
 import Comment from "../components/templates/Comment";
 import HeaderTop from "../components/templates/HeaderTop";
@@ -11,6 +12,8 @@ const PostPage = () => {
   const [post, setPost] = useState([]);
   const [comments, setComments] = useState([]);
 
+  const { register, handleSubmit } = useForm({ defaultValues: { body: "", post: "" } });
+
   useEffect(() => {
     axios.get(`${BASE_URL}v1/post/${postId}`).then(({ data }) => {
       setPost(data.post);
@@ -18,14 +21,23 @@ const PostPage = () => {
     });
   }, []);
 
+  const onSubmit = (data) => {
+    const info = { ...data, post: postId };
+
+    axios
+      .post(`${BASE_URL}v1/comment`, info, { headers: { Authorization: `Bearer ${TOKEN_ADMIN}` } })
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="my-20">
+    <div className="mb-20">
       <HeaderTop />
       <main className="container">
         <PostDetails post={post} id="post" />
         <div className="mt-10" id="comments">
           <h1 className="text-3xl font-DanaDemiBold mb-10">نظرات</h1>
-          <form className="flex flex-col items-start gap-5">
+          <form className="flex flex-col items-start gap-5" onSubmit={handleSubmit(onSubmit)}>
             <textarea
               name=""
               id=""
@@ -33,11 +45,12 @@ const PostPage = () => {
               rows="10"
               placeholder="نظر خود را بنویسید"
               className="bg-gray-200/50 py-2.5 px-5 rounded-lg outline-none w-full tb:w-2/3 h-32"
+              {...register("body", { required: true, minLength: 20, maxLength: 1000 })}
             ></textarea>
             <input type="submit" value="ثبت نظر" className="btn" />
           </form>
         </div>
-        <ul className="w-full tb:w-2/3 mt-10">
+        <ul className="w-full tb:w-2/3 mt-10 grid gap-5">
           {comments.map((comment) => (
             <Comment key={comment._id} {...comment} />
           ))}
